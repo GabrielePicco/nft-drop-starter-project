145f881582d81100e8b56ac18e8d58e850a5d8cf
Gabriele Picco <piccogabriele@gmail.com>
2022-01-05 12:23:24 +0100

    Change base url

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL
+});
 
 ReactDOM.render(
   <React.StrictMode>
